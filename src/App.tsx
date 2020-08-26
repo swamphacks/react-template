7f76ce75ceca418d69081020ba4c9b3a82b35616
Zachary Cowan <cz8ch8ry@gmail.com>
2020-08-26 14:56:38 -0400

    feat(layout): added fixed sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,75 @@
 import React, { FC } from 'react';
-// This file should handle the routing
+import { Layout, Menu } from 'antd';
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UserOutlined,
+  UploadOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+
 import Home from './pages/Home';
+import './App.css';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 const App: FC = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Layout>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+        }}
+      >
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            nav 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            nav 2
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UploadOutlined />}>
+            nav 3
+          </Menu.Item>
+          <Menu.Item key="4" icon={<BarChartOutlined />}>
+            nav 4
+          </Menu.Item>
+          <Menu.Item key="5" icon={<CloudOutlined />}>
+            nav 5
+          </Menu.Item>
+          <Menu.Item key="6" icon={<AppstoreOutlined />}>
+            nav 6
+          </Menu.Item>
+          <Menu.Item key="7" icon={<TeamOutlined />}>
+            nav 7
+          </Menu.Item>
+          <Menu.Item key="8" icon={<ShopOutlined />}>
+            nav 8
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout
+        className="site-layout"
+        style={{ marginLeft: 200, minHeight: '100vh' }}
+      >
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content
+          style={{ margin: '24px 16px 0', overflow: 'initial', height: '100%' }}
+        >
+          <div className="site-layout-background" style={{ padding: 24 }}>
+            <Home />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>SwampHacks</Footer>
+      </Layout>
+    </Layout>
   );
 };
 
