c416d91fa46c34e559cb38906a57b875f401d665
SwampHacks Tech <68782560+swamphacksTech@users.noreply.github.com>
2020-08-26 14:31:38 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+// This file should handle the routing
+import Home from './pages/Home';
+
+const App: FC = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
